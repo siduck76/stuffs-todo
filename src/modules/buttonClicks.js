4274be162d87c864fb49821402afa1185b505c38
siduck76 <siduckk76@protonmail.com>
2021-02-04 21:07:54 +0530

    minor changes

--- src/modules/buttonClicks.js
+++ src/modules/buttonClicks.js
@@ -1,12 +1,28 @@
-import { inputProjectName, saveProjectNameBtn } from "./divSelectors";
+import {
+  inputProjectName,
+  saveProjectNameBtn,
+  pName,
+  listOfProjects,
+} from "./divSelectors";
+
 import { addProject_toList } from "./miscFunctions";
 
 let newProjectBtn = document.querySelector(".newProjectBtn");
 let addProject = document.querySelector(".addProject");
 
-newProjectBtn.addEventListener("click", () => {
-  addProject.style.display = "grid";
-});
+newProjectBtn.addEventListener("click", () => addProject.style.display = "grid");
+
+const p_onHover = () => {
+  listOfProjects = document.querySelectorAll(".project"); // updates the var
+
+  listOfProjects.forEach((project) => {
+    project.addEventListener("click",() => pName.innerText = project.lastElementChild.innerText);
+    project.addEventListener("mouseover",() => project.style.color = "#949cdf");
+    project.addEventListener("mouseout",() => project.style.color = "#6a6d7a");
+  });
+};
+
+// adds the project to the list
 
 saveProjectNameBtn.addEventListener("click", () => {
   let projectName = inputProjectName.value;
@@ -14,4 +30,8 @@ saveProjectNameBtn.addEventListener("click", () => {
 
   addProject.style.display = "none";
   inputProjectName.value = "";
+
+  p_onHover();
 });
+
+p_onHover();
--- src/modules/divSelectors.js
+++ src/modules/divSelectors.js
@@ -1,2 +1,6 @@
 export const saveProjectNameBtn = document.querySelector('#saveProjectNameBtn')
 export const inputProjectName = document.querySelector('#inputProjectName');
+export let pName = document.querySelector(".project_Name");
+export let listOfProjects = document.querySelectorAll(".project");
+
+
