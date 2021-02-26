e5616cc637d38036cbeb5088ef746999b8f41ab7
siduck76 <siduckk76@protonmail.com>
2021-02-26 20:10:07 +0530

    initial dark theme

--- src/modules/divSelectors.js
+++ src/modules/divSelectors.js
@@ -1,6 +1,7 @@
-export const saveProjectNameBtn = document.querySelector('#saveProjectNameBtn')
-export const inputProjectName = document.querySelector('#inputProjectName');
+export const saveProjectNameBtn = document.querySelector("#saveProjectNameBtn");
+export const inputProjectName = document.querySelector("#inputProjectName");
 export let pName = document.querySelector(".project_Name");
 export let listOfProjects = document.querySelectorAll(".project");
-
-
+export let todaysTodos = document.querySelector(".todaysTodos");
+export let projects = document.querySelectorAll(".projects");
+export let importantTodos = document.querySelector(".importantTodos");
--- src/modules/themeToggle.js
+++ src/modules/themeToggle.js
@@ -2,8 +2,42 @@
 
 let themeToggle_Btn = document.querySelector(".fa-adjust");
 
-const apply_DarkTheme = () => {};
+const apply_DarkTheme = () => {
+  // body
+
+  let body = document.querySelector("body");
+  body.classList.toggle("darkBody");
+
+  // topheading
+
+  let topH = document.querySelector(".topheading");
+  topH.classList.toggle("dark_topHeading");
+
+  // new project btn
+
+  let newProjectBtn = document.querySelector(".newProjectBtn");
+  newProjectBtn.classList.toggle("dark_newProjectBtn");
+
+  // sidebar
+
+  let sBar = document.querySelector(".sidebar");
+  sBar.classList.toggle("dark_sidebar");
+
+  let todaysTodos = document.querySelector(".todaysTodos");
+  let importantTodos = document.querySelector(".importantTodos");
+  let projects = document.querySelector(".projects");
+
+  [todaysTodos, importantTodos, projects].forEach((tmp) => {
+    tmp.addEventListener("mouseover", () => {
+      tmp.style.backgroundColor = "#1d2021";
+    });
+
+    tmp.addEventListener("mouseout", () => {
+      tmp.style.backgroundColor = "#181b1c";
+    });
+  });
+};
 
 themeToggle_Btn.addEventListener("click", () => {
-  alert("in-progress!");
+  apply_DarkTheme();
 });
