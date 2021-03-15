0f04a8953352dc5df9ae8e488567240577ebf55b
siduck76 <siduckk76@protonmail.com>
2021-03-15 19:11:23 +0530

    fix : unchecked todo not changing color

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import "./modules/buttonClicks"
+import "./modules/buttonClicks";
 
 import {
   retrieve_LocalStorage,
   showTodays_todo,
 } from "./modules/miscFunctions";
 
-import  "./modules/settingsPage";
-import  "./modules/themeToggle";
+/* import  "./modules/settingsPage";
+ import  "./modules/themeToggle"; */
 
 export let whichPro_Clicked = "default",
   todoList = [],
-  projectArr = [],
-  defaultTheme = "light";
+  projectArr = [];
+// defaultTheme = "light";
 
 if (localStorage.length > 0) retrieve_LocalStorage();
 
